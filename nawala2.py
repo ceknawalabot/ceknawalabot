338c3611f8ae225af7e744bd1b8f7dc1b1fecc2d
ceknawalabot <ravenclaw00783@gmail.com>
2024-10-04 09:09:11 +0700

    ganti ke index.html

--- nawala2.py
+++ nawala2.py
@@ -16,28 +16,29 @@ data = response.text
 domains = data.splitlines()
 
 # Step 4: Check if the keyword is in the list of domains
-output_html = "<html><body><h1>Nawala bot (Trust Positif Checker)</h1><ul>"
+index_html = "<html><body><h1>Nawala bot (Trust Positif Checker)</h1><ul>"
 
 while True:  # Tambahkan perulangan tak terbatas
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Nawala bot (Trust Positif Checker)")
     for keyword in keywords:
         if keyword in domains:
-            output_html += f"<li>{keyword}: <span style='color:red;'>Blocked</span></li>"
+            index_html += f"<li>{keyword}: <span style='color:red;'>Blocked</span></li>"
             print(f"{keyword}: \033[91mBlocked\033[0m")
         else:
-            output_html += f"<li>{keyword}: Not Blocked</li>"
+            index_html += f"<li>{keyword}: Not Blocked</li>"
     print(f"Last update checked: {time.ctime()}")
-    output_html += f"</ul><p>Last update checked: {time.ctime()}</p></body></html>"
+    index_html += f"</ul><p>Last update checked: {time.ctime()}</p></body></html>"
 
-    # Simpan output ke file HTML
-    with open('output.html', 'w') as f:
-        f.write(output_html)
+    # Simpan index ke file HTML
+    with open('index.html', 'w') as f:
+        f.write(index_html)
 
     # Push ke GitHub
     repo = git.Repo('.')  # Inisialisasi repository
-    repo.index.add(['output.html'])  # Tambahkan file output.html ke staging
-    repo.index.commit('Update output.html')  # Commit perubahan
+    repo.index.add(['index.html'])  # Tambahkan file index.html ke staging
+    repo.index.commit('Update index.html')  # Commit perubahan
+    repo.git.push('--set-upstream', 'origin', 'main')  # Setelah commit
     origin = repo.remote(name='origin')  # Ambil remote origin
     origin.push()  # Push ke GitHub
 
